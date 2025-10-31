471868947521dd83f27da1da44dabe869abe1dae
m <m@gmail.com>
2025-11-01 01:19:45 +0530

    postpage name eroor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
-import PostPage from './pages/PostPage';
+import Postpage from './pages/Postpage'; // <-- THIS IS THE FIX (lowercase 'p')
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import CreatePostPage from './pages/CreatePostPage';
-import UpdatePostPage from './pages/UpdatePostPage'; // <-- 1. ADD THIS IMPORT
+import UpdatePostPage from './pages/UpdatePostPage'; // (Assuming you named this file 'UpdatePostPage.jsx' as we discussed)
 
 function App() {
   return (
@@ -14,11 +14,11 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/post/:id" element={<PostPage />} />
+          <Route path="/post/:id" element={<Postpage />} /> {/* <-- Make sure this matches the import name */}
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/create" element={<CreatePostPage />} />
-          <Route path="/update/:id" element={<UpdatePostPage />} /> {/* <-- 2. ADD THIS ROUTE */}
+          <Route path="/update/:id" element={<UpdatePostPage />} />
         </Routes>
       </main>
     </div>
