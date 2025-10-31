b69a1030e76543f300bf303f4014697681a22dde
m <m@gmail.com>
2025-11-01 03:18:10 +0530

    Update deployment configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 1. Add this 'server' object
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1600,
+  },
   server: {
     proxy: {
-      // 2. Any request starting with '/api'
       '/api': {
-        // 3. Will be proxied to your backend server
-        target: 'http://localhost:5001',
-        // 4. This is needed for the server to accept the request
+        target: process.env.VITE_API_URL || 'http://localhost:5001',
         changeOrigin: true,
+        secure: false,
       },
     },
   },
